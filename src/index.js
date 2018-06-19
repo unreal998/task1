8ea831b49a2ac3b491ff0dad4a04222033047fba
unreal998 <31666287+unreal998@users.noreply.github.com>
2018-06-19 16:49:05 +0300

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,80 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import $ from "jquery" 
+import { timingSafeEqual } from "crypto";
+
+
+
+function getJson(url) {
+    return JSON.parse($.ajax({
+        type: 'GET',
+        url: url,
+        dataType: 'json',
+        global: false,
+        async: false,
+        success: function (data) {
+            return data;
+        }
+    }).responseText);
+}
+
+var result = getJson('https://randomuser.me/api/');
+console.log(result);
+    result = result.results;
+console.log(result);
+
+
+class UserStats extends React.Component{
+    render(){
+        return(
+            <div>
+                {
+                    result.map(function(el){
+                        return(
+                             <User key={el.id.name} pic={el.picture.large} gender={el.gender} title={el.name.title} name={el.name.first} last={el.name.last}  />
+                        )
+                    })
+                }
+            </div>
+        )
+    }
+}
+
+class User extends React.Component{
+    constructor(props){
+        super(props);
+        this.state = {name: this.props.name};
+        this.handleClick = this.handleClick.bind(this);
+    }
+    handleSearch(event) {
+        this.setState(prevState =>({
+            name: event.target.value}));
+    }
+
+   /* handleClick(){
+        this.setState(prevState =>({
+            name: "yolo"
+        }))
+    }*/
+    render(){
+        return(
+        <div>
+            <img src={this.props.pic}></img>   
+            <h2 className = "gender"> Gender: {this.props.gender}</h2>
+            <div className="name" >
+                <h2>Name: {this.props.title} {this.state.name} {this.props.last}</h2>
+            </div>
+            <div className= "change">
+                <input id="textArea" type="text" onChange={this.handleSearch}></input>
+                <input type="button" onClick={this.handleClick}></input>
+            </div>
+        </div>
+    )
+    }
+}
+
+
+ReactDOM.render(
+    <UserStats/>,
+    document.getElementById("app")
+);
---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+const path = require("path");
+const HtmlWepbackPlugin = require("html-webpack-plugin");
+
+
+module.exports = {
+    entry: "./src/index.js",
+    output: {
+        path: path.join(__dirname, "/dist"),
+        filename: "App.js"
+    },
+    module: {
+        rules:[
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use:{
+                    loader: "babel-loader"
+                }
+            }
+    ]
+    },
+    plugins: [
+        new HtmlWepbackPlugin({
+            template:"./src/index.html"
+        })
+    ]
+  }
